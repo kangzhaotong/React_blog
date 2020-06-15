31dae682d69336ad8b71cb0d773ae979def83f93
Tddtest <297854770@qq.com>
2020-06-15 22:28:01 +0800

    feat: 修复git仓库

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">测试ant</Button>
+    </div>
+  );
+}
+
+export default App;
